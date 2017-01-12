3ade7b4dd4ec57403b00f2d00c82ce4d7b11e64a
Cooper Dale <cz777cooper@gmail.com>
2017-01-12 10:00:43 +0100

    lepší hledání nových souborů a blbuvzdorné

--- IIS_logrotate.ps1
+++ IIS_logrotate.ps1
@@ -1,5 +1,5 @@
 clear
-$IIS_LogRotator_version = "1"
+$IIS_LogRotator_version = "2"
 
 # is log for site of per server?
 # netsh http flush logbuffer - vypsaní logů na disk
@@ -14,39 +14,44 @@ $IIS_LogRotator_version = "1"
 $IIS_log_path = "C:\inetpub\logs\LogFiles\" #lomítko na konci NUTNÉ
 $IIS_outdir = "M:\weby\Dropbox\old"
 $reg_basepath = "HKLM:\Software\BSS\IIS_logrotator"
+$file_filter = "*.log"
 
 
 IF(!(Test-Path $reg_basepath)) { New-Item -Path $reg_basepath -Force | Out-Null }
 New-ItemProperty -Path $reg_basepath -Name version -Value $IIS_LogRotator_version -PropertyType DWord -Force | Out-Null
 $today = (Get-Date -format "yyMMdd")
-$resetcounter = 0
+
 $IIS_sites = Get-ChildItem -path $IIS_log_path
 
-
-function Fmidnight
-	{
+function fNewfile {
 	$global:reg_instancepath = $reg_basepath + "\" + $IIS_instance
-	$today_old = (Get-ItemProperty -Path $reg_instancepath -Name LastDate -ErrorAction SilentlyContinue).LastDate
-	#Write-Host $today " a " $today_old
-	$day_delta = $today_old - $today
+	$file_old = (Get-ItemProperty -Path $reg_instancepath -Name LastFile -ErrorAction SilentlyContinue).LastFile
+	Write-Host "old file" $file_old
+	$latest = (Get-ChildItem -Path ($IIS_log_path + $IIS_instance) -Filter $file_filter | Sort-Object CreationTime -Descending | Select-Object -First 1).name
 	
-	IF($today_old - $today -eq -1) {
-		Write-Host $day_delta "Byla půlnoc"
-		IF(!(Test-Path $reg_instancepath)) { New-Item -Path $reg_instancepath -Force | Out-Null }
-        New-ItemProperty -Path $reg_instancepath -Name LastDate -Value $today -PropertyType STRING -Force | Out-Null
-	    $global:today = $today_old
+	IF(!(Test-Path $reg_instancepath)) { New-Item -Path $reg_instancepath -Force | Out-Null }
+	New-ItemProperty -Path $reg_instancepath -Name LastFile -Value $latest -PropertyType STRING -Force | Out-Null
+	
+	IF ( $file_old -ne $latest ) { 
+		Write-Host "v regu je starý soubor, tak se na něj ještě jednou podívám" $file_old "vs" $latest
+		$latest = $file_old
 		}
-	ELSE {
-		IF(!(Test-Path $reg_instancepath)) { New-Item -Path $reg_instancepath -Force | Out-Null }
-        New-ItemProperty -Path $reg_instancepath -Name LastDate -Value $today -PropertyType STRING -Force | Out-Null
-		}	
+	elseif (!$file_old) {
+		#Write-Host "nic v registru"
+		}
+	else {
+		#Write-Host "vše OK"
+		}
+	
+	$global:IISfile = $IIS_log_path + $IIS_instance + "\" + $latest
+	#Write-Host $IISfile
+	
+	#output
+	$global:IISoutfile = $IIS_outdir + "\IIS_" + $env:computername + "_" + $IIS_instance + "_" + $(Get-Date -format "yyMMdd-HHmmss") + ".log"
 	}
 
 function Counting
 	{
-	$global:IISfile = $IIS_log_path + $IIS_instance + "\u_ex" + $today + ".log"
-	$global:IISoutfile = $IIS_outdir + "\IIS_" + $env:computername + "_" + $IIS_instance + "_" + $(Get-Date -format "yyMMdd-HHmmss") + ".log"
-    Write-Host $IISfile
 	$global:lines_old = (Get-ItemProperty -Path $reg_instancepath -Name Counter -ErrorAction SilentlyContinue).Counter
 	$global:lines_act = (Get-Content $IISfile | Measure-Object -Line |Select-Object Lines).Lines
 	$global:lines_delta = $lines_act - $lines_old
@@ -69,27 +74,25 @@ function FlushLogbuffer {
 		Write-Host "jede flash log buffer" $multi
 		}
 	}
-
-
-foreach ($IIS_instance in $IIS_sites) {
-# Write-Host $IISfile $IIS_instance
-Fmidnight
-Counting
-
-if ($lines_delta -gt 0) {
-	Foutput
 	
-	}
-elseif ($lines_delta -lt 0) {
-	Write-Host "nesedí počty řádků, žádný výstup, resetuji počítadlo pro" $IIS_instance
-	New-ItemProperty -Path $reg_instancepath -Name Counter -Value 0 -PropertyType STRING -Force | Out-Null
-	}
-else {
-	Write-Host "no new lines = no output file $IISoutfile"
-	$no_lines = (Get-ItemProperty -Path $reg_basepath -Name NoLines).NoLines
-	$no_lines ++
-	IF(!(Test-Path $reg_basepath)) { New-Item -Path $reg_basepath -Force | Out-Null }
-    New-ItemProperty -Path $reg_basepath -Name NoLines -Value $no_lines -PropertyType DWORD -Force | Out-Null
-	}
-	}
+foreach ($IIS_instance in $IIS_sites) {
+	# Write-Host $IISfile $IIS_instance
+	fNewFile
+	Counting
+
+	if ($lines_delta -gt 0) {
+		Foutput
+		}
+	elseif ($lines_delta -lt 0) {
+		Write-Host "nesedí počty řádků, žádný výstup, resetuji počítadlo pro" $IIS_instance
+		New-ItemProperty -Path $reg_instancepath -Name Counter -Value 0 -PropertyType STRING -Force | Out-Null
+		}
+	else {
+		Write-Host "no new lines = no output file $IISoutfile"
+		$no_lines = (Get-ItemProperty -Path $reg_basepath -Name NoLines -ErrorAction SilentlyContinue ).NoLines
+		$no_lines ++
+		IF(!(Test-Path $reg_basepath)) { New-Item -Path $reg_basepath -Force | Out-Null }
+	    New-ItemProperty -Path $reg_basepath -Name NoLines -Value $no_lines -PropertyType DWORD -Force | Out-Null
+		}
+		}
 FlushLogbuffer
